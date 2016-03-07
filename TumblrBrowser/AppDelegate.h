112986b8a51565ce20da931b42f8997c70f04b45
Magdalena Łazarecka <magdalenalazarecka@pawels-macbook-air.local>
2016-03-07 12:06:24 +0100

    Initial Commit

---
+++ TumblrBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TumblrBrowser
+//
+//  Created by Magdalena Łazarecka on 3/7/16.
+//  Copyright © 2016 Magdalena Łazarecka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TumblrBrowser/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TumblrBrowser
+//
+//  Created by Magdalena Łazarecka on 3/7/16.
+//  Copyright © 2016 Magdalena Łazarecka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
